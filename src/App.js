e73155b540f5ce4ad7750f87b581255bceb6dd34
Natalia Novoa <natalianovoa2022@u.northwestern.edu>
2020-01-26 21:58:36 -0600

    styled static catalog page with images

--- src/App.js
+++ src/App.js
@@ -1,4 +1,57 @@
+
 import React, { useEffect, useState } from 'react';
+import ReactDOM from "react-dom";
+
+import "rbx/index.css";
+import { Card, Title, Content, Footer, Image, Column, Group } from "rbx";
+
+const ProductList = ({products}) => {
+    return products.map(product => {
+      return <Card style={{ width: '20rem'}}>
+         <Card.Image>
+            <Image.Container>
+              <Image src={"data/products/" + product.sku + "_2.jpg"} />
+            </Image.Container>
+          </Card.Image> 
+
+          <Card.Content>
+          <Title>{product.title}</Title>
+          <Title subtitle>{"$" + product.price}</Title>
+          </Card.Content>
+
+        <Card.Footer as="footer" size={128}>
+
+          <Card.Footer.Item as="p">
+            <span>
+              <a href="#">S</a>
+            </span>
+          </Card.Footer.Item>
+          <Card.Footer.Item as="p">
+            <span>
+              <a href="#">M</a>
+            </span>
+          </Card.Footer.Item>
+          <Card.Footer.Item as="p">
+            <span>
+              <a href="#">L</a>
+            </span>
+          </Card.Footer.Item>
+          <Card.Footer.Item as="p">
+            <span>
+              <a href="#large">XL</a>
+            </span>
+          </Card.Footer.Item>
+        </Card.Footer>
+        <Card.Footer as="footer">
+          <Card.Footer.Item as="a">
+            <span>
+              <a href="#add">Add to Cart</a>
+            </span>
+          </Card.Footer.Item>
+        </Card.Footer>
+      </Card> 
+})
+}
 
 const App = () => {
   const [data, setData] = useState({});
@@ -13,12 +66,11 @@ const App = () => {
       setData(json);
     };
     fetchProducts();
-  }, []);
-
+  }, [])
+  
   return (
     <ul>
-      
-      {products.map(product => <li key={product.sku}>{product.title}</li>)}
+      <ProductList products={products}/>
     </ul>
   );
 };

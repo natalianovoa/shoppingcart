dae19b148e7125c7071fa32256bdbf9848ab3e09
Natalia Novoa <natalianovoa2022@u.northwestern.edu>
2020-01-27 20:56:54 -0600

    Styled website

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import ReactDOM from "react-dom";
 import Product from './product.js';
-
 import "rbx/index.css";
-import { Card, Title, Content, Footer, Image, Column, Group } from "rbx";
-import {Button, Container, Col, Row} from 'react-bootstrap';
+import { Card, Title, Content, Footer, Image, Column, Group, Button, Navbar, Item, Brand, Burger, Linl, Dropdown, Divider,
+Segment, Field, Control, Menu, Icon} from "rbx";
 
 const App = () => {
   const [data, setData] = useState({});
@@ -18,11 +17,44 @@ const App = () => {
     fetchProducts();
   }, []);
   return( 
-  <Column.Group vcentered multiline> 
+  <div>
+    <Navbar color="light">
+  <Navbar.Brand>
+    <Navbar.Item href="https://new-shopping-car.firebaseapp.com/">
+      <Title>Shopping Cart</Title>
+    </Navbar.Item>
+    <Navbar.Burger />
+  </Navbar.Brand>
+  <Navbar.Menu>
+    <Navbar.Segment align="start">
+      <Navbar.Item href="https://new-shopping-car.firebaseapp.com/">Home</Navbar.Item>
+      <Navbar.Item dropdown hoverable>
+        <Navbar.Link href="https://new-shopping-car.firebaseapp.com/">
+          Shopping Cart
+        </Navbar.Link>
+      </Navbar.Item>
+    </Navbar.Segment>
+
+    <Navbar.Segment align="end">
+      <Navbar.Item as="div">
+        <Field kind="group">
+          <Control>
+            <Button as="a" color="primary" href="https://twitter.com">
+              <span>Sign Up</span>
+            </Button>
+          </Control>
+        </Field>
+      </Navbar.Item>
+    </Navbar.Segment>
+  </Navbar.Menu>
+</Navbar>
+
+  <Column.Group vcentered multiline gapSize={2}> 
             {products.map(product =>
               <Product product={product}/>
             )}
   </Column.Group>
+  </div>
   )
 };
 export default App;
--- src/product.js
+++ src/product.js
@@ -2,40 +2,35 @@ import React, { useEffect, useState } from 'react';
 import ReactDOM from "react-dom";
 
 import "rbx/index.css";
-import { Card, Title, Content, Footer, Image, Column, Group } from "rbx";
-import {Button, Container, Col, Row} from 'react-bootstrap';
+import { Card, Title, Content, Footer, Image, Column, Group, Button } from "rbx";
+import { Container, Col, Row} from 'react-bootstrap';
 
 
 const Product = ({product}) => {
       return (
           <Column  size="one-quarter">
             <Card size="15px">
-            <Image.Container size="4by5"> <Card.Image textAlign="centered"> <Image src={"data/products/" + product.sku + "_2.jpg"} size={18} /> 
+            <Image.Container size="4by5"> <Card.Image textAlign="centered"> <img src={"data/products/" + product.sku + "_2.jpg"} width="50"
+            height="28"/> 
             </Card.Image> </Image.Container>
             <Card.Content size="4by2">
                 <Title size={4} spaced textAlign="centered" >{product.title}</Title>
                 <Title subtitle textAlign="centered">{product.currencyFormat} {product.price}</Title>
             </Card.Content>
                 <Card.Footer as="footer" size={128}>
-
                     <Card.Footer.Item as="p">
-                        <Button variant="primary">S</Button>
+                        <Button.Group>
+                        <Button outlined color="primary" key="white">S</Button>
+                        <Button outlined color="primary" key="white">M</Button>
+                        <Button outlined color="primary" key="white">L</Button>
+                        <Button outlined color="primary" key="white">XL</Button>
+                        </Button.Group>
                     </Card.Footer.Item>
-                    <Card.Footer.Item as="p">
-                        <Button variant="primary">M</Button>
-                    </Card.Footer.Item>
-                    <Card.Footer.Item as="p">
-                        <Button variant="primary">L</Button>
-                    </Card.Footer.Item>
-                    <Card.Footer.Item as="p">
-                        <Button variant="primary">XL</Button>
-                    </Card.Footer.Item>
-        
                 </Card.Footer>
 
                 <Card.Footer as="footer">
                     <Card.Footer.Item as="a">
-                        <Button variant="primary">Add to Cart</Button>
+                        <Button color="primary">Add to Cart</Button>
                     </Card.Footer.Item>
                 </Card.Footer>
             </Card>
